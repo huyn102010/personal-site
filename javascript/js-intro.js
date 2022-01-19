d584607474e39a87e3bd6c50791cbc2e0c784c25
Huy Nguyen <huyn102010@gmail.com>
2022-01-19 09:49:43 -0500

    Example JS file organization

--- javascript/js-intro.js
+++ javascript/js-intro.js
@@ -2,12 +2,37 @@
 
 // Global Variables
 
+var importantData = {
+  so: 'important'
+};
 
 // Function Statements, class Statements
+var SomeFancyFunction = function(){
+  // Here we get fancy
+}
+
+var someFancyClass = class(){
+  //Create a fancy class
+}
+
+var someOldSchoolClass = function(){
+  // Create some old school class
+}
 
 
 
-// Down to business
 
+
+// Down to business, call function that we defined
+
+var myFancyClass = new someFancyClass();
+var myOldSchoolClass = new someOldSchoolClass();
+
+someFancyFunction(importantData) 
+
+someWorkHorseFunction(); // Hoisting 
 
 // Function Expression 
+function someWorkHorseFunction(){
+  // And here we get down to work.. 
+}
