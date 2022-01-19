dcc3961ddd58104d0d990eb2f103f68409b6262f
Huy Nguyen <huyn102010@gmail.com>
2022-01-19 09:42:30 -0500

    Add JS starter file with general layout of code

---
+++ javascript/js-intro.js
@@ -0,0 +1,13 @@
+'use strict'; // to force browswer enforce stricter rules
+
+// Global Variables
+
+
+// Function Statements, class Statements
+
+
+
+// Down to business
+
+
+// Function Expression 
