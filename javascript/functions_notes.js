9e3edd1a171049a80833d996b714cb9f8c12afaa
Huy Nguyen <huyn102010@gmail.com>
2022-01-19 10:13:46 -0500

    Add case examples and makeAdder func

--- javascript/functions_notes.js
+++ javascript/functions_notes.js
@@ -1,9 +1,23 @@
 'use strict';
 
+
+//SCREAMCASE   : constant
+//SCREAMING_SNAKE_CASE: constant
+//TitleCase : classes
+//camelCase : functions
+// ---
+// snake_case: variables (values)
+
+
+
+
 // Demonstrate functions as value; do not use func() when passed in as argument
 function doSomeThing(func,data){
   func(data);
 }
+
+
+
 
 
 // Closure
@@ -16,6 +30,16 @@ function doSomethingLater(data){
 var myNewLaterFunction = doSomeThingLater('Hank offer sleep lessons for cheap');
 myNewLaterFunction() // shoudl return I have some data: Hank offer sleep lessons for cheap
 
+
+
+//More closure example
+function makeAdder(num){
+  return function(x){
+    return x + num;
+  }
+};
+var addTwo = makeAdder(2);
+addTwo(7) // shoudl return 9;
 
 
 
